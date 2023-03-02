8c5c00f41d1f388070440963917d3fc872449ad9
Joe van Bohemen <joe.vanbohemen@roli.com>
2023-03-02 17:12:04 +0000

    presentation ready

--- src/local.ts
+++ src/local.ts
@@ -13,31 +13,35 @@ void (async function () {
     switch (arg) {
       case "addUser":
         await userStorage.addUser({
-          email: "joevanbo@gmail.com",
+          email: "joe@gmail.com",
           password: "abc123",
         });
         break;
       case "updateUser":
         await userStorage.updateUser({
           id: 1,
-          email: "joevanbo@gmail.com",
+          email: "joe@gmail.com",
           password: "UPDATED",
         });
         break;
       case "deleteUser":
         await userStorage.deleteUser(1);
         break;
-      case "rateLimit":
+      case "add100Users":
+        const promises: Promise<any>[] = [];
         for (let i = 0; i < 100; i++) {
-          await userStorage.addUser({
-            email: "joevanbo@gmail.com",
+          const p = userStorage.addUser({
+            email: `joe+${i}@gmail.com`,
             password: "abc123",
           });
+          promises.push(p);
         }
+        await Promise.all(promises);
         break;
     }
   } catch (e) {
     console.error(e);
     console.error("failed");
+    process.exit(1);
   }
 })();
--- test/addSheetRow.js
+++ test/addSheetRow.js
@@ -1,14 +1,18 @@
 const axios = require("axios");
 
 const url = "https://mfj0zjjx8j.execute-api.eu-west-2.amazonaws.com/v1/sheets";
+const data = {
+  email: "joevanbo+localtest@gmail.com",
+  password: "abc123",
+};
+
+console.log("POST >", url);
+console.log(data);
 
 axios({
   method: "post",
   url,
-  data: {
-    email: "joevanbo+localtest@gmail.com",
-    password: "abc123",
-  },
+  data,
 })
   .then((r) => {
     console.log(r.data);
